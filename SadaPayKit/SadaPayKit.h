12841b6ce7706a31d6d0c21bb904b184d2430c73
Muhammad Hussaan Saeed <mhs@192.168.10.19>
2023-01-20 17:05:11 +0500

    Add SadaPayKit framework and target

---
+++ SadaPayKit/SadaPayKit.h
@@ -0,0 +1,18 @@
+//
+//  SadaPayKit.h
+//  SadaPayKit
+//
+//  Created by Muhammad Hussaan Saeed on 1/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SadaPayKit.
+FOUNDATION_EXPORT double SadaPayKitVersionNumber;
+
+//! Project version string for SadaPayKit.
+FOUNDATION_EXPORT const unsigned char SadaPayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SadaPayKit/PublicHeader.h>
+
+
